64428be0a3d0ef906d6275fd4f4c3cbe2bcad013
DahnCodes <stanleydaniel994@gmail.com>
2024-04-23 13:08:34 +0100

    i made use of javascript strings

---
+++ script.js
@@ -0,0 +1,20 @@
+let text = "    My Name is Daniel   "
+let text1 = text.trimEnd();
+console.log(text1);
+
+let word = "Daniel"
+let word1 = word.toLocaleUpperCase();
+console.log(word1);
+
+let text2 = "Javascript is the best"
+let result = text2.repeat(2);
+console.log(result);
+
+let name = "STANLEY"
+let output = word.toLocaleLowerCase();
+console.log(output);
+
+let session = "Year One"
+let year = session.charAt(2);
+console.log(year);
+
